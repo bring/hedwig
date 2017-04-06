5a69e3037e354c3827ecf061752dbc598f90dd3c
Fredrik Jensen <fredrik@netliferesearch.com>
2017-04-06 22:19:49 +0200

    only open one port on Heroku

--- server.js
+++ server.js
@@ -39,24 +39,28 @@ assets.use(function(req, res, next) {
  */
 
 docs.use(express.static(`${__dirname}/docs`));
-build.use(express.static(`${__dirname}/build`));
-assets.use(express.static(`${__dirname}/assets`));
-
-/**
- * Attach servers to port
- */
 
 docs.listen(portDocs, () => {
   console.log(`Docs is running on port ${portDocs} 🎉`);
   console.log(`Open docs at http://localhost/${portDocs}`);
 });
 
-build.listen(portBuild, () => {
-  console.log(`Docs is running on port ${portBuild} 🎉`);
-  console.log(`Open docs at http://localhost/${portBuild}`);
-});
+/**
+ * Start other servers if in develeop
+ */
 
-assets.listen(portAssets, () => {
-  console.log(`Docs is running on port ${portAssets} 🎉`);
-  console.log(`Open docs at http://localhost/${portAssets}`);
-});
+if (process.env.NODE_ENV !== 'production') {
+  build.use(express.static(`${__dirname}/build`));
+
+  build.listen(portBuild, () => {
+    console.log(`Docs is running on port ${portBuild} 🎉`);
+    console.log(`Open docs at http://localhost/${portBuild}`);
+  });
+
+  assets.use(express.static(`${__dirname}/assets`));
+
+  assets.listen(portAssets, () => {
+    console.log(`Docs is running on port ${portAssets} 🎉`);
+    console.log(`Open docs at http://localhost/${portAssets}`);
+  });
+}
