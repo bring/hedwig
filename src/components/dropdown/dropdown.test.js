f0de57d351df19368b458ee95efc0565fe8d7ee2
bard <bnhovde@gmail.com>
2017-04-18 13:28:44 +0200

    FEATURE - Added basic tests for dropdown

--- src/components/dropdown/dropdown.test.js
+++ src/components/dropdown/dropdown.test.js
@@ -1,7 +1,36 @@
-import HWDropdown from './dropdown';
+/* global test expect document */
 
-const dropdown = HWDropdown();
+// Set up our document body
+document.body.innerHTML =
+  `<div class="hw-dropdown"
+    data-hw-dropdown="characters"
+    data-hw-dropdown-placeholder="Choose option"
+    data-hw-dropdown-default-selected="2"
+  >
+    <div class="hw-dropdown__inner">
+      <div class="hw-dropdown__placeholder"></div>
+      <div class="hw-dropdown__arrow"></div>
+      <ul class="hw-dropdown__options">
+        <li class="hw-dropdown__option" data-hw-dropdown-value="1">Harry</li>
+        <li class="hw-dropdown__option" data-hw-dropdown-value="2">Hedwig</li>
+        <li class="hw-dropdown__option" data-hw-dropdown-value="3">Fredrik</li>
+      </ul>
+    </div>
+  </div>`;
 
-test('Initialises successfully', () => {
-  expect(dropdown.toBe(3));
+require('./dropdown');
+
+test('Dropdown initialises if selector found', () => {
+  const dropdown = document.querySelectorAll('.hw-dropdown')[0];
+  const status = dropdown.getAttribute('data-hw-dropdown-initialised');
+
+  expect(status).toEqual('true');
 });
+
+test('Dropdown selects default value if found', () => {
+  const defaultOption = document.querySelectorAll('[data-hw-dropdown-value="2"]')[0];
+  const selected = defaultOption.getAttribute('data-hw-dropdown-option-selected');
+
+  expect(selected).toEqual('true');
+});
+
