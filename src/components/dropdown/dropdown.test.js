0f56d1a89171232880c6851361a3dfae00be0c40
bard <bnhovde@gmail.com>
2017-04-18 13:39:12 +0200

    FEATURE - More tests for dropdown

--- src/components/dropdown/dropdown.test.js
+++ src/components/dropdown/dropdown.test.js
@@ -20,6 +20,7 @@ document.body.innerHTML =
 
 require('./dropdown');
 
+
 test('Dropdown initialises if selector found', () => {
   const dropdown = document.querySelectorAll('.hw-dropdown')[0];
   const status = dropdown.getAttribute('data-hw-dropdown-initialised');
@@ -27,10 +28,38 @@ test('Dropdown initialises if selector found', () => {
   expect(status).toEqual('true');
 });
 
+
 test('Dropdown selects default value if found', () => {
   const defaultOption = document.querySelectorAll('[data-hw-dropdown-value="2"]')[0];
   const selected = defaultOption.getAttribute('data-hw-dropdown-option-selected');
 
   expect(selected).toEqual('true');
+});
+
+
+test('Dropdown opens on click', () => {
+  const dropdown = document.querySelectorAll('.hw-dropdown')[0];
+  dropdown.click();
+
+  const list = dropdown.querySelectorAll('.hw-dropdown__options')[0];
+  const status = list.getAttribute('aria-hidden');
+
+  expect(status).toEqual('false');
+});
+
+
+test('Selected value updates when option selected', () => {
+  const dropdown = document.querySelectorAll('.hw-dropdown')[0];
+  dropdown.click();
+
+  const firstItem = dropdown.querySelectorAll('.hw-dropdown__option')[0];
+  const secondItem = dropdown.querySelectorAll('.hw-dropdown__option')[1];
+  firstItem.click();
+
+  const firstItemSelected = firstItem.getAttribute('data-hw-dropdown-option-selected');
+  const secondItemSelected = secondItem.getAttribute('data-hw-dropdown-option-selected');
+
+  expect(firstItemSelected).toEqual('true');
+  expect(secondItemSelected).toEqual('false');
 });
 
