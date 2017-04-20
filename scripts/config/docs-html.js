e838811fe5524ef72d8ce948ffcf024af3aaac91
Fredrik Jensen <fredrik@netliferesearch.com>
2017-04-20 16:40:11 +0200

    fixed :bug:

--- scripts/config/docs-html.js
+++ scripts/config/docs-html.js
@@ -1,9 +1,30 @@
 
-var production = process.env.NODE_ENV !== 'production';
+var production = process.env.NODE_ENV === 'production';
 
-var reload = '';
+var reload = '<script src="//localhost:9091"></scripts>';
+
+var icons = `
+  <script>
+    var url = 'http://localhost:3001/assets/svg-sprite.svg';
+
+    var ajax = new XMLHttpRequest();
+
+    ajax.open("GET", url, true);
+    ajax.send();
+
+    ajax.onload = function(e) {
+      var div = document.createElement("div");
+      div.style.display = 'none';
+      div.innerHTML = ajax.responseText;
+      document.body.insertBefore(div, document.body.childNodes[0]);
+    }
+
+  </script>
+`;
+
 if (production) {
-  reload = '<script src="//localhost:9091"></scripts>';
+  reload = null;
+  icons = 'https://hedwig-cdn.s3.amazonaws.com/hedwig/icons.js';
 }
 
 
@@ -44,22 +65,7 @@ module.exports = `
       <div id="catalog" />
       <script src="https://interactivethings.github.io/catalog/catalog.min.js"></script>
       <script src="docs-config.js"></script>
-      <script>
-        var url = 'http://localhost:3001/assets/svg-sprite.svg';
-
-        var ajax = new XMLHttpRequest();
-
-        ajax.open("GET", url, true);
-        ajax.send();
-
-        ajax.onload = function(e) {
-          var div = document.createElement("div");
-          div.style.display = 'none';
-          div.innerHTML = ajax.responseText;
-          document.body.insertBefore(div, document.body.childNodes[0]);
-        }
-
-      </script>
+      ${icons}
       ${reload}
     </body>
     </html>
