62a87aef6cb5415b2686a486ef65dd351bb34a08
leifriksheim <riksheim.leif@gmail.com>
2017-07-18 12:11:06 +0200

    FEATURE: Added input validators.

--- src/shared/components/input/Input.md
+++ src/shared/components/input/Input.md
@@ -43,14 +43,75 @@ All inputs are block elements. For layout, use [grid]('/Grid').
 
 ## Validation
 For inline validation we use:
+**data-hw-validate="type"**
 
 ```html
 <label class="hw-label">
-  Label
-  <input type="text" class="hw-input"  data-hw-validate="text" placeholder="Type 4 letters to test validation" />
+  Email
+  <input type="text"
+         class="hw-input"  
+         data-hw-validate="email"
+         placeholder="Type email for validation" />
   <div class="hw-error hw-error--is-hidden" data-hw-error-message>E-postadressen er ugyldig</div>
 </label>
+<label class="hw-label">
+  Postal code
+  <input type="text"
+         maxlength="4"
+         class="hw-input"  
+         data-hw-validate="postal-code"
+         placeholder="Type postal code for validation" />
+  <div class="hw-error hw-error--is-hidden" data-hw-error-message>Postnr er ugyldig</div>
+</label>
+<label class="hw-label">
+  Telefonnummer
+  <div class="hw-grid hw-grid--narrow">
+    <div class="hw-grid__item hw-one-third">
+      <label class="hw-label">
+        <select
+          data-hw-dropdown="numbers"
+          data-hw-dropdown-small>
+          <option value="1">+47</option>
+          <option value="2">+48</option>
+          <option value="3">+49</option>
+        </select>
+      </label>
+    </div><!--
+    --><div class="hw-grid__item hw-two-thirds">
+        <input class="hw-input"
+               type="text"
+               maxlength="8"
+               data-hw-validate="phone-number"
+               placeholder="00000000" />
+        <div class="hw-error hw-error--is-hidden" data-hw-error-message>Telefonnummer må inneholde 8 nummber</div>
+    </div>
+  </div>
+  <label class="hw-label">
+    Visa Card (see other cards under)
+    <input type="text"
+           class="hw-input"  
+           data-hw-validate="credit-card"
+           data-hw-validate-card-type="Visa"
+           placeholder="Type Visa card number for validation" />
+    <div class="hw-error hw-error--is-hidden" data-hw-error-message>Kedittkortnummer er ugyldig</div>
+  </label>
+</label>
 ```
-```hint|neutral
-Validation test
+
+## Validators
+
+```code
+[Email]                [data-hw-validate="email"]
+[Phone]                [data-hw-validate="phone-number"]
+[Postal code]          [data-hw-validate="postal-code"]
+[Card]                 [data-hw-validate="card"]
+```
+
+### Credit cards (combine with card validator)
+
+```code
+[Visa]                 [data-hw-validate-card-type="Visa"]
+[Mastercard]           [data-hw-validate-card-type="MaterCard"]
+[Diners Club]           [data-hw-validate-card-type="MasterCard"]
+[American Express]     [data-hw-validate-card-type="AmEx"]
 ```
--- src/shared/components/input/validation.js
+++ src/shared/components/input/validation.js
@@ -1,39 +1,110 @@
 /**
  * Validation
  */
+import q from '../../utilities/js/q';
+import qa from '../../utilities/js/qa';
+import { checkCreditCard } from '../input/creditcard';
 
-/**
- * Find all inputs with validation
- */
 
-const input = document.querySelectorAll('[data-hw-validate]')[0];
-const message = document.querySelectorAll('[data-hw-error-message]')[0];
+const HWValidate = ({
+    validateSelector = '[data-hw-validate]',
+    errorMessageSelector = '[data-hw-error-message]',
+  } = {}) => {
+  // Module settings object
+  const SETTINGS = {
+    elements: qa(validateSelector), // All input DOM nodes
+    errors: qa(errorMessageSelector),
+  };
 
-/**
- * If there is any validation inputs
- */
-function validateTextInput(e) {
-  const value = e.target.value;
-
- /**
-  * Add error class and remove hidden class for validation messages
-  */
-
-  if (value.length > 3) {
-    input.classList.add('hw-input--error');
-    message.classList.remove('hw-error--is-hidden');
-  } else {
-    input.classList.remove('hw-input--error');
-    message.classList.add('hw-error--is-hidden');
+  function validateEmail(e) {
+    const value = e.target.value;
+    const input = e.target;
+    const message = e.target.nextElementSibling;
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(value)) {
+      input.classList.remove('hw-input--error');
+      message.classList.add('hw-error--is-hidden');
+    } else {
+      input.classList.add('hw-input--error');
+      message.classList.remove('hw-error--is-hidden');
+    }
   }
-}
 
-if (input) {
-  /**
-   * Add the listener
-   */
+  function validatePostalCode(e) {
+    const value = e.target.value;
+    const input = e.target;
+    const message = e.target.nextElementSibling;
+    if (!/\D/.test(value) && value.length === 4) {
+      input.classList.remove('hw-input--error');
+      message.classList.add('hw-error--is-hidden');
+    } else {
+      input.classList.add('hw-input--error');
+      message.classList.remove('hw-error--is-hidden');
+    }
+  }
 
-  input.addEventListener('input', validateTextInput);
-}
+  function validatePhoneNumber(e) {
+    const value = e.target.value;
+    const input = e.target;
+    const message = e.target.nextElementSibling;
+    if (!/\D/.test(value) && value.length === 8) {
+      input.classList.remove('hw-input--error');
+      message.classList.add('hw-error--is-hidden');
+    } else {
+      input.classList.add('hw-input--error');
+      message.classList.remove('hw-error--is-hidden');
+    }
+  }
 
-export default validateTextInput;
+  function validateCardNumber(e) {
+    const value = e.target.value;
+    const input = e.target;
+    const cardType = input.getAttribute('data-hw-validate-card-type');
+    const message = e.target.nextElementSibling;
+    if (checkCreditCard(value, cardType)) {
+      input.classList.remove('hw-input--error');
+      message.classList.add('hw-error--is-hidden');
+    } else {
+      input.classList.add('hw-input--error');
+      message.classList.remove('hw-error--is-hidden');
+    }
+  }
+
+  function bindEvents(trigger, validateType) {
+    if (validateType === 'email') {
+      trigger.addEventListener('input', validateEmail);
+    }
+    if (validateType === 'postal-code') {
+      trigger.addEventListener('input', validatePostalCode);
+    }
+    if (validateType === 'phone-number') {
+      trigger.addEventListener('input', validatePhoneNumber);
+    }
+    if (validateType === 'credit-card') {
+      trigger.addEventListener('input', validateCardNumber);
+    }
+  }
+
+  function init() {
+    if (SETTINGS.elements.length < 1) {
+      return;
+    }
+    // Loop through all toggles and initialise each
+    SETTINGS.elements.forEach((input) => {
+      // Skip if already initialised
+      if (input.getAttribute('data-hw-validate-initialised') === 'true') { return false; }
+
+      // Mark as initialised
+      input.setAttribute('data-hw-validate-initialised', true);
+
+      const validateType = input.getAttribute('data-hw-validate');
+
+      // Set up event listeners
+      bindEvents(input, validateType);
+    });
+  }
+
+// Initialise HWValidate component
+  init();
+};
+
+HWValidate();
