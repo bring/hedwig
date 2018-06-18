0c5b96a7839fb0022990e7c210fed90dd4ef34b9
leifriksheim <riksheim.leif@gmail.com>
2018-06-19 00:33:36 +0200

    Fixed navbar test

--- src/bring/components/navbar/tests/navbar.test.js
+++ src/bring/components/navbar/tests/navbar.test.js
@@ -1,31 +1,313 @@
 /* global test expect document */
 
 document.body.innerHTML = `
-  <header class="navbar">
-    <div class="navbar__logo">
-      <svg>
-        <use xlink:href="#logo"></use>
-      </svg>
-    </div>
-    <nav class="navbar__menu">
-      <a href="#" class="navbar__item">English</a>
-      <a href="#" class="navbar__item">Login</a>
-      <button class="navbar__button">
-        Search
-        <svg class="hw-icon">
-          <use xlink:href="#search"></use>
+<header class="hw-navbar">
+  <div class="hw-navbar__content">
+
+    <div class="hw-navbar__top">
+      <a href="#" class="hw-navbar__logo">
+        <svg class="hw-navbar__logo-standard" aria-labelledby="Logo" role="img">
+          <title id="Logo">Go to homepage</title>
+          <use xlink:href="#logo-bring"></use>
         </svg>
-      </button>
-      <button class="navbar__button" data-hw-toggle-menu>
-        Menu
-        <div class="hw-hamburger" data-hw-menu-icon>
-          <span class="hw-hamburger__part"></span>
-          <span class="hw-hamburger__part"></span>
-          <span class="hw-hamburger__part"></span>
+        <svg class="hw-navbar__logo-white" aria-labelledby="Logo" role="img">
+          <title id="Logo">Go to homepage</title>
+          <use xlink:href="#logo-bring-white"></use>
+        </svg>
+      </a>
+
+      <nav class="hw-navbar__menu">
+        <button class="hw-navbar__item" data-hw-drawer-trigger="login">Logg inn</button>
+        <button class="hw-navbar__search-button" data-hw-toggle-search>
+          Søk
+          <svg class="hw-navbar__search-icon">
+            <use xlink:href="#search"></use>
+          </svg>
+        </button>
+        <button class="hw-navbar__menu-button" data-hw-toggle-menu>
+          <span>Meny</span>
+          <span>Lukk</span>
+          <div class="hw-hamburger" data-hw-menu-icon>
+            <span class="hw-hamburger__part"></span>
+            <span class="hw-hamburger__part"></span>
+            <span class="hw-hamburger__part"></span>
+          </div>
+        </button>
+      </nav>
+    </div>
+
+    <div class="hw-drawer" data-hw-drawer="login">
+      <div class="hw-drawer__overlay hw-animate hw-animate--fade-in-overlay" data-hw-drawer-trigger="Drawer2"></div>
+      <div class="hw-drawer__content">
+        <div class="hw-drawer__header">
+          <div class="hw-drawer__close">
+            <button class="hw-drawer__close-button" data-hw-drawer-trigger="login">
+              <svg class="hw-icon" role="img" aria-labelledby="close-drawer">
+                 <title id="close-drawer">Close drawer</title>
+                 <use xlink:href="#cross-thin"></use>
+              </svg>
+            </button>
+          </div>
         </div>
-      </button>
-    </nav>
-  </header>
+        <div class="hw-drawer__body">
+          <div class="hw-block hw-block--mb hw-text-center">
+            <svg class="hw-icon--gray-lighter" role="img" aria-labelledby="avatar">
+            <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#avatar"></use>
+          </svg>
+          </div>
+          <div class="hw-block hw-block--mb">
+            <h2 class="hw-text-center">Hvor vil du logge inn?</h2>
+          </div>
+          <hr class="hw-hr hw-hr--centered">
+          <div class="hw-block hw-block--mt">
+            <div class="hw-container hw-container--narrow">
+              <a class="hw-button hw-button--secondary hw-button--large hw-button--full" href="#">
+                Mybring
+              </a>
+              <a class="hw-button hw-button--secondary hw-button--large hw-button--full" href="#">
+                Min post
+              </a>
+              <a class="hw-button hw-button--secondary hw-button--large hw-button--full" href="#">
+                Frimerkebutikken
+              </a>
+              <a class="hw-button hw-button--secondary hw-button--large hw-button--full" href="#">
+                Digipost
+              </a>
+              <a class="hw-button hw-button--secondary hw-button--large hw-button--full" href="#">
+                MINe - Internasjonalt gods
+              </a>
+              <a class="hw-button hw-button--secondary hw-button--large hw-button--full" href="#">
+                MINe - Offshore &amp; Energy
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <section class="hw-navbar__drawer" data-hw-menu-drawer>
+      <div class="hw-navbar__drawer-content">
+
+        <div class="hw-navbar__drawer-mobile">
+
+          <div class="hw-search hw-search--standalone"
+            data-hw-search
+            data-hw-search-placeholders='["Søk"]'
+          >
+            <div class="hw-search__inner">
+              <input
+                data-hw-search-input
+                class="hw-search__input" type="text"
+                aria-label="Search" />
+              <svg class="hw-search__icon">
+                <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#search"></use>
+              </svg>
+              <span class="hw-search__placeholder" data-hw-search-placeholder></span>
+            </div>
+          </div>
+
+          <div class="hw-block hw-block--full hw-block--mt-large">
+            <ul class="hw-accordion hw-accordion--dark" data-hw-accordion="example3" data-hw-accordion-allow-multiple>
+              <li class="hw-accordion__item">
+                <button class="hw-accordion__trigger">
+                  Sende
+                  <div class="hw-accordion__arrow"></div>
+                </button>
+                <div class="hw-accordion__contents">
+                  <ul class="hw-accordion__link-items">
+                    <li class="hw-accordion__link-item">
+                      <a href="#" class="hw-accordion__link">Post</a>
+                    </li>
+                    <li class="hw-accordion__link-item">
+                      <a href="#" class="hw-accordion__link">Pakker</a>
+                    </li>
+                  </ul>
+                </div>
+              </li>
+              <li class="hw-accordion__item">
+                <button class="hw-accordion__trigger">
+                  Motta
+                  <div class="hw-accordion__arrow"></div>
+                </button>
+                <div class="hw-accordion__contents">
+                  <ul class="hw-accordion__link-items">
+                    <li class="hw-accordion__link-item">
+                      <a href="#" class="hw-accordion__link">Spore en forsendelse</a>
+                    </li>
+                    <li class="hw-accordion__link-item">
+                      <a href="#" class="hw-accordion__link">
+                        Adresseendring og oppbevaring av post
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </li>
+              <li class="hw-accordion__item">
+                <button class="hw-accordion__trigger">
+                  Rådgivning
+                  <div class="hw-accordion__arrow"></div>
+                </button>
+                <div class="hw-accordion__contents">
+                 <ul class="hw-accordion__link-items">
+                    <li class="hw-accordion__link-item">
+                      <a href="#" class="hw-accordion__link">Lagerløsninger</a>
+                    </li>
+                    <li class="hw-accordion__link-item">
+                      <a href="#" class="hw-accordion__link">Netthandel</a>
+                    </li>
+                  </ul>
+                </div>
+              </li>
+            </ul>
+          </div>
+
+        </div>
+
+        <div class="hw-navbar__drawer-desktop">
+          <div class="hw-grid">
+            <div class="hw-grid__item hw-one-half hw-medium--one-quarter">
+
+              <h2 class="hw-navbar__title">Sende</h2>
+              <hr class="hw-hr">
+
+              <div class="hw-navbar__menu-group">
+                <h3 class="hw-navbar__menu-group-title">Post</h3>
+                <ul class="hw-navbar__menu-group-links">
+                  <li>
+                    <a href="#">Med like formater</a>
+                  </li>
+                  <li>
+                    <a href="#">Med blandede formater</a>
+                  </li>
+                </ul>
+              </div>
+
+              <div class="hw-navbar__menu-group">
+                <h3 class="hw-navbar__menu-group-title">Pakker</h3>
+                <ul class="hw-navbar__menu-group-links">
+                  <li>
+                    <a href="#">Med like formater</a>
+                  </li>
+                  <li>
+                    <a href="#">Med blandede formater</a>
+                  </li>
+                </ul>
+              </div>
+
+            </div><!--
+            --><div class="hw-grid__item hw-one-half hw-medium--one-quarter">
+              <h2 class="hw-navbar__title">Motta</h2>
+              <hr class="hw-hr">
+
+              <div class="hw-navbar__menu-group">
+                <h3 class="hw-navbar__menu-group-title">Post</h3>
+                <ul class="hw-navbar__menu-group-links">
+                  <li>
+                    <a href="#">Med like formater</a>
+                  </li>
+                  <li>
+                    <a href="#">Med blandede formater</a>
+                  </li>
+                </ul>
+              </div>
+
+              <div class="hw-navbar__menu-group">
+                <h3 class="hw-navbar__menu-group-title">Pakker</h3>
+                <ul class="hw-navbar__menu-group-links">
+                  <li>
+                    <a href="#">Med like formater</a>
+                  </li>
+                  <li>
+                    <a href="#">Med blandede formater</a>
+                  </li>
+                </ul>
+              </div>
+
+            </div><!--
+            --><div class="hw-grid__item hw-one-half hw-medium--one-quarter">
+              <h2 class="hw-navbar__title">Rådgivning</h2>
+              <hr class="hw-hr">
+
+              <div class="hw-navbar__menu-group">
+                <h3 class="hw-navbar__menu-group-title">Post</h3>
+                <ul class="hw-navbar__menu-group-links">
+                  <li>
+                    <a href="#">Med like formater</a>
+                  </li>
+                  <li>
+                    <a href="#">Med blandede formater</a>
+                  </li>
+                </ul>
+              </div>
+
+              <div class="hw-navbar__menu-group">
+                <h3 class="hw-navbar__menu-group-title">Pakker</h3>
+                <ul class="hw-navbar__menu-group-links">
+                  <li>
+                    <a class="hw-navbar__link" href="#">Med like formater</a>
+                  </li>
+                  <li>
+                    <a href="#">Med blandede formater</a>
+                  </li>
+                </ul>
+              </div>
+
+            </div><!--
+            --><div class="hw-grid__item hw-one-half hw-medium--one-quarter">
+              <h2 class="hw-navbar__title">Kundeservice</h2>
+              <hr class="hw-hr">
+
+              <div class="hw-navbar__menu-group">
+                <h3 class="hw-navbar__menu-group-title">Post</h3>
+                <ul class="hw-navbar__menu-group-links">
+                  <li>
+                    <a href="#">Med like formater</a>
+                  </li>
+                  <li>
+                    <a href="#">Med blandede formater</a>
+                  </li>
+                </ul>
+              </div>
+
+              <div class="hw-navbar__menu-group">
+                <h3 class="hw-navbar__menu-group-title">Pakker</h3>
+                <ul class="hw-navbar__menu-group-links">
+                  <li>
+                    <a href="#">Med like formater</a>
+                  </li>
+                  <li>
+                    <a href="#">Med blandede formater</a>
+                  </li>
+                </ul>
+              </div>
+
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </section>
+    <div class="hw-navbar__overlay" data-hw-navbar-overlay></div>
+    <section class="hw-navbar__search">
+      <div class="hw-search hw-search--large"
+        data-hw-search
+      >
+        <span class="hw-search__background"></span>
+        <div class="hw-search__inner">
+          <input
+            data-hw-search-input
+            class="hw-search__input" type="text"
+            aria-label="Søk"
+            placeholder="Søk" />
+          <svg class="hw-search__icon">
+            <use xlink:href="#search"></use>
+          </svg>
+          <span class="hw-search__placeholder" data-hw-search-placeholder></span>
+        </div>
+      </div>
+    </section>
+  </div>
+</header>
 `;
 
 /**
@@ -35,21 +317,16 @@ document.body.innerHTML = `
 require('../navbar');
 
 /**
- * Insert text to input field and trig change
- */
-
-const menuButtonSelector = document.body.querySelectorAll('[data-hw-toggle-menu]')[0];
-const menuButtonIconSelector = document.body.querySelectorAll('[data-hw-menu-icon]')[0];
-const activeClass = 'hw-hamburger--active';
-
-/**
  * Check results
  */
 
-test('If added class to menu button button', () => {
+test('If added class to menu button', () => {
+  const menuButtonSelector = document.body.querySelectorAll('[data-hw-toggle-menu]')[0];
+  const menuButtonIconSelector = document.body.querySelectorAll('[data-hw-menu-icon]')[0];
+  const activeClass = 'hw-hamburger--active';
+
   menuButtonSelector.click();
 
   const hasAddedActiveClass = menuButtonIconSelector.classList.contains(activeClass);
-
   expect(hasAddedActiveClass).toEqual(true);
 });
--- src/posten/components/navbar/tests/navbar.test.js
+++ src/posten/components/navbar/tests/navbar.test.js
@@ -1,23 +1,25 @@
 /* global test expect document */
 
 document.body.innerHTML = `
-  <header class="navbar">
-    <div class="navbar__logo">
-      <svg>
-        <use xlink:href="#logo"></use>
+<header class="hw-navbar">
+  <div class="hw-navbar__content">
+    <a href="#" class="hw-navbar__logo">
+      <svg class="hw-navbar__logo-standard">
+        <use xlink:href="#logo-posten"></use>
       </svg>
-    </div>
-    <nav class="navbar__menu">
-      <a href="#" class="navbar__item">English</a>
-      <a href="#" class="navbar__item">Login</a>
-      <button class="navbar__button">
-        Search
-        <svg class="hw-icon">
+      <svg class="hw-navbar__logo-white">
+        <use xlink:href="#logo-posten-white"></use>
+      </svg>
+    </a>
+    <nav class="hw-navbar__menu">
+      <button class="hw-navbar__search-button" data-hw-toggle-search>
+        Søk
+        <svg class="hw-navbar__search-icon">
           <use xlink:href="#search"></use>
         </svg>
       </button>
-      <button class="navbar__button" data-hw-toggle-menu>
-        Menu
+      <button class="hw-navbar__menu-button" data-hw-toggle-menu>
+        <span>Meny</span>
         <div class="hw-hamburger" data-hw-menu-icon>
           <span class="hw-hamburger__part"></span>
           <span class="hw-hamburger__part"></span>
@@ -25,7 +27,28 @@ document.body.innerHTML = `
         </div>
       </button>
     </nav>
-  </header>
+    <section class="hw-navbar__drawer" data-hw-menu-drawer></section>
+    <div class="hw-navbar__overlay" data-hw-navbar-overlay></div>
+    <section class="hw-navbar__search">
+      <div class="hw-search hw-search--large"
+        data-hw-search
+        data-hw-search-placeholders='["Søk etter pakke", "Eks.: 1212012381238123", "Eller på nettstedet", "Eks.: Bedriftspakke"]'
+      >
+        <span class="hw-search__background"></span>
+        <div class="hw-search__inner">
+          <input
+            data-hw-search-input
+            class="hw-search__input" type="text"
+            aria-label="Search" />
+          <svg class="hw-search__icon">
+            <use xlink:href="#search"></use>
+          </svg>
+          <span class="hw-search__placeholder" data-hw-search-placeholder></span>
+        </div>
+      </div>
+    </section>
+  </div>
+</header>
 `;
 
 /**
