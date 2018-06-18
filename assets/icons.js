348600790baddefdb639184ab0c957cfcbb0622b
leifriksheim <riksheim.leif@gmail.com>
2018-06-18 15:06:58 +0200

    Get icons from it's own reference

--- assets/icons.js
+++ assets/icons.js
@@ -1,9 +1,19 @@
-const url = 'https://bring-hedwig.s3.amazonaws.com/hedwig/assets/svg-sprite.svg';
+function replaceLastPathofUrl(url, dest) {
+  const split = url.split('/');
+  split[split.length - 1] = dest;
+  return split.join('/');
+}
 
+// Self reference the script tag and get the sprite
+const src = document.getElementById('hw-icons').src;
+const spriteUrl = replaceLastPathofUrl(src, 'svg-sprite.svg');
+
+// Load svg-sprite with ajax
+// https://css-tricks.com/ajaxing-svg-sprite/
 function loadIcons() {
   const ajax = new XMLHttpRequest();
 
-  ajax.open('GET', url, true);
+  ajax.open('GET', spriteUrl, true);
   ajax.send();
 
   ajax.onload = function (e) {
