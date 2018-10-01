c478c8c619c8b5f51fbd3577d64b60d73a7b7d4f
Viggo Normann <v@iggo.no>
2018-10-01 13:14:51 +0200

    Add polyfill for remove() for IE9 and up

---
+++ src/shared/utilities/js/polyfills/node-remove.js
@@ -0,0 +1,20 @@
+/**
+ * Polyfill for node.remove() for Internet Explorer 9 and up
+ * https://developer.mozilla.org/en-US/docs/Web/API/ChildNode/remove#Browser_compatibility#Polyfill
+ */
+(function (arr) {
+  arr.forEach(function (item) {
+    if (item.hasOwnProperty('remove')) {
+      return;
+    }
+    Object.defineProperty(item, 'remove', {
+      configurable: true,
+      enumerable: true,
+      writable: true,
+      value: function remove() {
+        if (this.parentNode !== null)
+          this.parentNode.removeChild(this);
+      }
+    });
+  });
+})([Element.prototype, CharacterData.prototype, DocumentType.prototype]);
