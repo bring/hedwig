089f5748baf2a9c0a1efd01c118f14a72e1a82cf
Viggo Normann <v@iggo.no>
2019-01-15 12:58:13 +0100

    Make sure Pikaday elements are only initialized once Also update to the latest version of Pikaday

--- src/shared/components/datepicker/datepicker-example.js
+++ src/shared/components/datepicker/datepicker-example.js
@@ -1,13 +1,15 @@
-if(document.getElementById('datepicker-ajsd4ks8')) {
-  const datepickerWrapper = document.getElementById('datepicker-ajsd4ks8');
-  var picker = new Pikaday({ 
+const datepickerWrapper = document.getElementById('datepicker-ajsd4ks8');
+if(datepickerWrapper && !datepickerWrapper.dataset.initialized) {
+  datepickerWrapper.dataset.initialized = true;
+  var picker = new Pikaday({
     container: datepickerWrapper,
     field: datepickerWrapper.querySelector('input'),
   });
 };
 
-if(document.getElementById('datepicker-ajsd4ks9')) {
-  const datepickerWrapper2 = document.getElementById('datepicker-ajsd4ks9');
+const datepickerWrapper2 = document.getElementById('datepicker-ajsd4ks9');
+if(datepickerWrapper2 && !datepickerWrapper2.dataset.initialized) {
+  datepickerWrapper2.dataset.initialized = true;
   var picker2 = new Pikaday({
     container: datepickerWrapper2,
     field: datepickerWrapper2.querySelector('input'),
