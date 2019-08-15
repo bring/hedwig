ecb51dfc573af336537d6048edba36d691c9bdd5
Viggo Normann <v@iggo.no>
2019-08-15 16:46:40 +0200

    Make postcss-preset-env work
    - Update and use the config file
    - Move custom media queries outside :root

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,19 @@
 const postcssPresetEnv = require('postcss-preset-env');
 
 module.exports = {
+  use: [
+    'postcss-import',
+    'postcss-preset-env',
+    'postcss-nested',
+    'postcss-extend',
+  ],
   plugins: [
-    postcssPresetEnv(/* pluginOptions */)
-  ]
-}
+    require('postcss-import'),
+    require('postcss-preset-env'),
+    require('postcss-nested'),
+    require('postcss-extend'),
+    postcssPresetEnv({ 
+      stage: 0, 
+    }),
+  ],
+};
