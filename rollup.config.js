b0b31830608bd58af3beceea4994f983fe6bb8be
Viggo Normann <v@iggo.no>
2022-12-23 15:09:21 +0100

    Remove building examples.js, server.js

--- rollup.config.js
+++ rollup.config.js
@@ -10,12 +10,8 @@ import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import { uglify } from 'rollup-plugin-uglify';
 
-
 export default [{
-  input: {
-    include: ['src/shared/utilities/js/polyfills/*.js', 'src/**/*.js'],
-    exclude: ['src/**/*.test.js', 'src/**/*.example.js'],
-  },
+  input: { include: ['src/shared/utilities/js/polyfills/*.js', 'src/**/*.js'] },
   onwarn: (warning) => {
     // Skip certain warnings
     if (warning.code === 'THIS_IS_UNDEFINED') {
@@ -40,33 +36,5 @@ export default [{
       include: 'node_modules/**',
     }),
     uglify(),
-  ],
-}, {
-  input: {
-    include: ['src/**/*.example.js'],
-  },
-  onwarn: (warning) => {
-    // Skip certain warnings
-    if (warning.code === 'THIS_IS_UNDEFINED') {
-      return;
-    }
-    // console.warn everything else
-    console.warn(warning.message);
-  },
-  output: {
-    file: 'dist/examples.js',
-    format: 'iife',
-  },
-  plugins: [
-    resolve(),
-    multiEntry(),
-    commonjs(),
-    babel({
-      exclude: 'node_modules/**',
-      runtimeHelpers: true,
-    }),
-    commonjs({
-      include: 'node_modules/**',
-    }),
   ],
 }];
